623168598ffe2951eb661b2e36315dec1846e5f6
derbuihan <derbuihan@gmail.com>
2025-02-01 15:24:41 +0900

    Implement save and load functions for NSW index; update main to handle multiple index files and measure query performance

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <sys/time.h>
+#include <time.h>
 
 #include "load_datasets.h"
 #include "vector.h"
@@ -15,40 +17,94 @@ void set_vector_store_from_dataset(VectorStore *store, Dataset *dataset) {
   }
 }
 
+void save_nsw_index(Dataset *dataset, char *filename, StoreType store_type) {
+  VectorStore *store = new_vector_store(store_type);
+  set_vector_store_from_dataset(store, dataset);
+
+  FILE *fp = fopen(filename, "wb");
+  save_vector_store(store, fp);
+  fclose(fp);
+}
+
+VectorStore *load_nsw_index(char *filename) {
+  FILE *fp = fopen(filename, "rb");
+  VectorStore *store = load_vector_store(fp);
+  fclose(fp);
+  return store;
+}
+
 int main(void) {
   char *awsdocs = "../datasets/awsdocs.parquet";
   char *querydocs = "../datasets/query.parquet";
 
-  // Setup VectorStore
-  Dataset *dataset = load_dataset(awsdocs);
-  VectorStore *store = new_vector_store(STORE_NSW);
-  set_vector_store_from_dataset(store, dataset);
+  StoreType types[] = {STORE_LINEAR, STORE_NSW};
+  char *index_files[] = {"awsdocs.linear.index", "awsdocs.nsw.index"};
+
+  // Save Indexes
+  // Dataset *dataset = load_dataset(awsdocs);
+  // for (int i = 0; i < 2; i++) {
+  //   save_nsw_index(dataset, index_files[i], types[i]);
+  // }
+
+  // Load Indexes
+  VectorStore *stores[2];
+  for (int i = 0; i < 2; i++) {
+    stores[i] = load_nsw_index(index_files[i]);
+  }
 
   // Setup Query
   Dataset *query_dataset = load_dataset(querydocs);
-  Record *query_record = &query_dataset->records[0];
-  Vector *query = new_vector(query_record->embedding_size);
-  set_data_vector(query, query_record->embedding);
-  printf("Query: %s\n", query_record->url);
+  Vector *query[query_dataset->num_records];
+  int num_queries = query_dataset->num_records;
+  for (int i = 0; i < num_queries; i++) {
+    Record *record = &query_dataset->records[i];
+    query[i] = new_vector(record->embedding_size);
+    set_data_vector(query[i], record->embedding);
+  }
 
   // Search
   int top_k = 10;
   int result_ids[top_k];
   float result_dists[top_k];
-  search_vectors(store, query, top_k, result_ids, result_dists);
 
-  // Print Results
-  for (int i = 0; i < top_k; i++) {
-    Record *record = &dataset->records[result_ids[i]];
-    printf("Results: id: %d, distance: %f, url: %s\n", result_ids[i],
-           result_dists[i], record->url);
+  for (int i = 0; i < 2; i++) {
+    VectorStore *store = stores[i];
+
+    struct timeval start, end;
+    clock_t start_t, end_t;
+    double wall_time, cpu_time;
+
+    gettimeofday(&start, NULL);
+    start_t = clock();
+
+    for (int j = 0; j < num_queries; j++) {
+      search_vectors(store, query[j], top_k, result_ids, result_dists);
+    }
+
+    end_t = clock();
+    gettimeofday(&end, NULL);
+
+    wall_time = (double)(end.tv_sec - start.tv_sec) +
+                (double)(end.tv_usec - start.tv_usec) / 1000000;
+    cpu_time = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+
+    printf("Index: %s\n", index_files[i]);
+    printf("Total queries: %d\n", query_dataset->num_records);
+    printf("Wall time: %.3f seconds (%.3f ms per query)\n", wall_time,
+           wall_time / query_dataset->num_records * 1000);
+    printf("CPU time: %.3f seconds (%.3f ms per query)\n", cpu_time,
+           cpu_time / query_dataset->num_records * 1000);
   }
 
   // Free Memory
-  free_dataset(dataset);
-  free_vector_store(store);
-  free_vector(query);
+  // free_dataset(dataset);
+  for (int i = 0; i < 2; i++) {
+    free_vector_store(stores[i]);
+  }
   free_dataset(query_dataset);
+  for (int i = 0; i < num_queries; i++) {
+    free_vector(query[i]);
+  }
 
   return 0;
 }
